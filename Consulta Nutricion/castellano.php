9403dd724ff500751b49628dde05f2d26cc5d4df
Nacho <imaciasm@ieslavereda.es>
2020-04-06 00:50:01 +0200

    Primer Commit: Estructura basica

---
+++ Consulta Nutricion/castellano.php
@@ -0,0 +1,93 @@
+<?php
+
+$titPagina="Consulta Nutrición online";
+
+/*Navegador*/
+$navServicios="Servicios";
+$navArticulos="Articulos";
+$navSobreMi="Sobre mi";
+$navPideCita="Pide cita";
+$navContacto="Contacto";
+$navIdioma="Idioma";
+$navEntrar="Entrar / Registrar";
+
+/*Cabecera*/
+$cabTitulo1="Amorchita Nutrición";
+$cabTitulo2="Consulta nutrición ónline";
+$cabBoton="Saber más";
+
+/*Servicios*/
+$serTitulo1="Servicios";
+$serTitulo2="Conoce todo lo que podemos hacer por ti en nuestro centro. Te ayudaremos a mejorar y llevar un estilo de vida saludable.";
+
+$serServicio1="Nutrición Deportiva";
+$serTexto1="Nos centramos en optimizar el rendimiento de las personas en los entrenamientos y competiciones, conseguir mejorar sus marcas al igual que mejorar la recuperación después de cada entrenamiento.";
+
+$serServicio2="Pérdida De Peso";
+$serTexto2="La pérdida de peso es un objetivo que puede resultar tedioso en algunas ocasiones, por ello, buscamos la consecución de este objetivo desde la adherencia ante los nuevos hábitos.";
+
+$serServicio3="Educación Nutricional";
+$serTexto3="Nuestro objetivo es enseñar los pilares básicos de la nutrición y ayudar a corregir los hábitos que están siendo perjudiciales. Queremos que comer sano se convierta en tu alternativa preferida.";
+
+$serServicio4="Psiconutrición";
+$serTexto4="Disciplina centrada en mejorar tus hábitos alimentarios y tu relación con la comida. Aquí, psicología y nutrición irán de la mano, para poder conseguir tus objetivos y que estos, perduren en el tiempo.";
+
+$serBoton="Conoce todos los servicios";
+
+/*Articulos*/
+$artTitulo1="Articulos";
+$artTitulo2="Últimos articulos de ínteres";
+
+$artBoton="Todos los articulos";
+
+//Articulos Modales
+$artFecha="Fecha";
+$artCategoria="Categoria";
+
+/*Sobre mi*/
+$sobTitulo1="Sobre mí";
+$sobTitulo2="Dietista - Nutricionista";
+
+$sobNombre="Zaira C. Barrado";
+
+$sobInfo1="Especializada en fitoterapia y dietética nutricional. Técnico en farmacia y parafarmacia";
+$sobInfo2="La mayor parte de mi experiencia en atención al cliente la he adquirido estos últimos años, trabajando en empresas muy competitivas las cuales se han preocupado de la formación en distintos campos y en los que he aprendido a trabajar en equipo, asesoramiento, responsabilidad entre otras cualidades.
+Mis objetivos y valores personales están muy ligados a la rama sanitaria porque siempre me ha interesado ayudar a los demás, por eso opté por una carrera sanitaria y me encantaría trabajar con una empresa comprometida con esos mismos valores.";
+
+/*Pide cita*/
+$pidTitulo1="Pide cita";
+$pidTitulo2="Consigue tu cambio en 4 simples pasos";
+
+$pidPaso1="REGISTRATE";
+$pidTexto1="texto para la parte de registrarse";
+
+$pidPaso2="PIDE CITA";
+$pidTexto2="texto para la parte de pedir cita";
+
+$pidPaso3="HABLAMOS";
+$pidTexto3="texto para la parte de hablamos";
+
+$pidPaso4="OBTEN RESULTADOS!";
+$pidTexto4="texto para la parte de obten resultados";
+
+$pidBoton="QUIERO <br> COMENZAR <br> YA!";
+
+/*Contacto*/
+$conTitulo1="Contactame";
+$conTitulo2="Preguntame cualquier duda por resolver";
+
+$conPlaceNombre="Tu nombre *";
+$conPlaceEmail="Tu email *";
+$conPlaceTelefono="Tu numero de telefono *";
+$conPlaceMensaje="Tu mensaje *";
+
+$conRequeridoNombre="Introduce tu nombre.";
+$conRequeridoEmail="Introduce tu correo electronico.";
+$conRequeridoTelefono="Introduce tu numero de telefono.";
+$conRequeridoMensaje="Introduce tu mensaje o duda.";
+
+$conBoton="Enviar mensaje";
+
+/*Footer*/
+$fooPolitica="Politica de privacidad";
+$fooTerminos="Terminos de Uso";
