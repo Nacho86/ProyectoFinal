da84f912b3fe142dbae39a125df45a7355029031
Nacho <imaciasm@ieslavereda.es>
2020-05-22 22:39:51 +0200

    Añadido documentacion de proyecto terminada

--- ConsultaNutricion/comentarios.php
+++ ConsultaNutricion/comentarios.php
@@ -3,16 +3,15 @@ require_once "Connection.php";
 require_once "comentario.php";
 date_default_timezone_set('UTC');
 
-if ($_SERVER["REQUEST_METHOD"]=="POST" )
-{
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
-	$num_art = $_POST["num_art"];
-	$nom_usu = $_POST["nom_usu"];
-	$tex_com = $_POST["tex_com"];
-	$fecha = date(d-m-Y H:i);
+    $num_art = $_POST["num_art"];
+    $nom_usu = $_POST["nom_usu"];
+    $tex_com = $_POST["tex_com"];
+    $fecha = date("d-m-Y H:i");
 
 
-	/*preparacion de la conexion*/
+    /*preparacion de la conexion*/
 
     $dbh = Connection::make();
 
@@ -25,9 +24,9 @@ if ($_SERVER["REQUEST_METHOD"]=="POST" )
     $stmt->bindParam(2, $nom_usu);
     $stmt->bindParam(3, $tex_com);
     $stmt->bindParam(4, $fecha);
-   
 
-     /*ejecutamos todo lo preparado anteriormente*/
+
+    /*ejecutamos todo lo preparado anteriormente*/
     $stmt->execute();
 
     if (isset($_POST['num_art'])) {
@@ -39,8 +38,5 @@ if ($_SERVER["REQUEST_METHOD"]=="POST" )
         $resultado = $stmt->fetchAll();
 
 
-
-
-
-
+    }
 }
